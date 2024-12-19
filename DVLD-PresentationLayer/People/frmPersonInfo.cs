69a4b63003389e902f68c29102725ed093d9a928
eslambob44 <eyasser237@gmail.com>
2024-12-19 17:58:39 +0200

    Implement frmPersonInfo

---
+++ DVLD-PresentationLayer/People/frmPersonInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_PresentationLayer.People
+{
+    public partial class frmPersonInfo : Form
+    {
+        public frmPersonInfo(int PersonID)
+        {
+
+            InitializeComponent();
+            ctrlShowPersonInfo1.Find(PersonID);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
