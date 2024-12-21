986755449c554f1de93543309df185f4533698f2
eslambob44 <eyasser237@gmail.com>
2024-12-21 16:34:26 +0200

    Implement clsCountry in business layer

---
+++ DVLD-BusinessLayer/clsCountry.cs
@@ -0,0 +1,29 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BusinessLayer
+{
+    static public class clsCountry
+    {
+        static public DataTable ListCountries()
+        {
+            return clsCountryDataAccessLayer.ListCountries();
+        }
+
+        static public int FindCountry(string CountryName)
+        {
+            return clsCountryDataAccessLayer.FindCountry(CountryName);
+        }
+
+        static public string FindCountry(int CountryId) 
+        {
+            return clsCountryDataAccessLayer.FindCountry(CountryId);
+        }
+
+    }
+}
