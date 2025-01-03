02aaca7aa50962a98a8b2adf475e3f7bc0f446be
eslambob44 <eyasser237@gmail.com>
2025-01-03 16:15:03 +0200

    Implement user login in business layer

---
+++ DVLD-BusinessLayer/clsUser.cs
@@ -0,0 +1,21 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BusinessLayer
+{
+    public class clsUser
+    {
+        static public int FindUser(string UserName , string Password)
+        {
+            return clsUsersDataAccessLayer.FindUser(UserName , Password);
+        }
+        static public bool IsUserActive(int UserId) 
+        {
+            return clsUsersDataAccessLayer.IsUserActive(UserId);
+        }
+    }
+}
