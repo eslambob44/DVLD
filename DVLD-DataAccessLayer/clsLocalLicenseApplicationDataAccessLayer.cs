435bac2ded452f38586f152825856e6cd01331b9
eslambob44 <eyasser237@gmail.com>
2025-01-27 16:16:08 +0200

    Implement List and find Local license Application

--- DVLD-DataAccessLayer/clsLocalLicenseApplicationDataAccessLayer.cs
+++ DVLD-DataAccessLayer/clsLocalLicenseApplicationDataAccessLayer.cs
@@ -94,5 +94,98 @@ namespace DVLD_DataAccessLayer
             }
             return ApplicationID;
         }
+
+        static public DataTable ListLocalLicenseApplications()
+        {
+            DataTable dtLicenseApplications = new DataTable();
+            SqlConnection Connection = new SqlConnection (clsDataAccessLayerSettings.ConnectionString);
+            string Query = @"Select [L.D.L.AppID] = LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID ,[Driving Class] = ClassName ,NationalNo 
+                            , FullName = (FirstName + ' '+SecondName + ' ' + ThirdName + ' '+LastName ) , ApplicationDate ,
+                            [Passed Tests] = 
+                            (
+                            	Select Count(Tests.TestResult) from Tests inner join TestAppointments 
+                            	on TestAppointments.TestAppointmentID = Tests.TestAppointmentID 
+                            	Where Tests.TestResult = 1 
+                            	and TestAppointments.LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID
+                            ) ,
+                            Status = 
+                            case
+                            	when ApplicationStatus = 1 then 'Now'
+                            	when ApplicationStatus = 3 then 'Completed'
+                            	when ApplicationStatus = 2 then 'Canceled'
+                            END
+                            From Applications
+                            inner join 
+                            (
+                            	LocalDrivingLicenseApplications inner join LicenseClasses 
+                            	on LocalDrivingLicenseApplications.LicenseClassID = LicenseClasses.LicenseClassID
+                            )
+                            on Applications.ApplicationID = LocalDrivingLicenseApplications.ApplicationID
+                            inner join People on Applications.ApplicantPersonID = People.PersonID
+                            ";
+            SqlCommand Command = new SqlCommand(Query, Connection);
+            try
+            {
+                Connection.Open();
+                SqlDataReader Reader = Command.ExecuteReader();
+                if(Reader .HasRows)
+                {
+                    dtLicenseApplications.Load(Reader);
+                }
+                Reader.Close ();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                Connection.Close ();
+            }
+            return dtLicenseApplications;
+        }
+
+        static public bool FindLocalLicenseApplication(int ldlApplication , ref int ApplicationID , ref int LicenseClassID,
+                                    ref int PersonID, ref DateTime ApplicationDate,
+                                    ref int ApplicationTypeID, ref short ApplicationStatus, ref DateTime LastStatusDate,
+                                    ref float PaidFees, ref int CreatedUserID)
+        {
+            bool IsFound = false;
+            SqlConnection Connection = new SqlConnection(clsDataAccessLayerSettings.ConnectionString);
+            string Query = @"Select * From LocalDrivingLicenseApplications
+                            Where LocalDrivingLicenseApplicationID = @ldlApplication";
+            SqlCommand command = new SqlCommand(Query, Connection);
+            command.Parameters.AddWithValue("@ldlApplication" , ldlApplication);
+            try
+            {
+                Connection.Open();
+                SqlDataReader Reader = command.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    ApplicationID = (int)Reader["ApplicationID"];
+                    LicenseClassID = (int)Reader["LicenseClassID"];
+                    if(clsApplicationDataAccessLayer.FindApplication(ApplicationID,ref PersonID, ref ApplicationDate,
+                        ref ApplicationTypeID, ref ApplicationStatus, ref LastStatusDate,
+                        ref PaidFees, ref CreatedUserID))
+                    {
+                        IsFound = true;
+                    }
+                }
+
+                Reader.Close ();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return IsFound;
+        }
+
+
     }
 }
