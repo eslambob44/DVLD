a29f63d6e5579fb7883e5accc82b3419dc557f36
eslambob44 <eyasser237@gmail.com>
2025-02-03 16:44:44 +0200

    implement license class data access and business layers

---
+++ DVLD-BusinessLayer/clsLicenseClass.cs
@@ -0,0 +1,27 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BusinessLayer
+{
+    static public class clsLicenseClass
+    {
+        static public float GetLicenseClassFees(int LicenseClassID)
+        {
+            return clsLicenseClassDataAccessLayer.GetLicenseClassFees(LicenseClassID);
+        }
+
+        static public int GetDefaultValidityLength(int LicenseClassID)
+        {
+            return clsLicenseClassDataAccessLayer.GetDefaultValidityLength(LicenseClassID);
+        }
+
+        static public int GetMinimumAllowedAge(int LicenseClassID)
+        {
+            return clsLicenseClassDataAccessLayer.GetMinimumAllowedAge(LicenseClassID);
+        }
+    }
+}
---
+++ DVLD-DataAccessLayer/clsLicenseClassDataAccessLayer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccessLayer
+{
+    static public class clsLicenseClassDataAccessLayer
+    {
+        static public float GetLicenseClassFees(int LicenseClassID)
+        {
+            float Fees = -1;
+            SqlConnection Connection = new SqlConnection(clsDataAccessLayerSettings.ConnectionString);
+            string Query = @"Select ClassFees From LicenseClasses Where LicenseClassID = @LicenseClassID";
+            SqlCommand Command = new SqlCommand(Query, Connection);
+            Command.Parameters.AddWithValue("@LicenseClassID" , LicenseClassID);
+            try
+            {
+                Connection.Open();
+                object Result = Command.ExecuteScalar();
+                if(Result != null && float.TryParse(Result.ToString() , out float Temp))
+                    Fees = Temp;
+            }
+            catch { }
+            finally { Connection.Close(); }
+            return Fees;
+        }
+
+
+        static public int GetDefaultValidityLength(int LicenseClassID)
+        {
+            int DefaultValidityLength = -1;
+            SqlConnection Connection = new SqlConnection(clsDataAccessLayerSettings.ConnectionString);
+            string Query = @"Select MinimumAllowedAge From LicenseClasses Where LicenseClassID = @LicenseClassID";
+            SqlCommand Command = new SqlCommand(Query, Connection);
+            Command.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+            try
+            {
+                Connection.Open();
+                object Result = Command.ExecuteScalar();
+                if (Result != null && int.TryParse(Result.ToString(), out int Temp))
+                    DefaultValidityLength = Temp;
+            }
+            catch { }
+            finally { Connection.Close(); }
+            return DefaultValidityLength;
+        }
+
+
+        static public int GetMinimumAllowedAge(int LicenseClassID)
+        {
+            int MinimumAllowedAge = -1;
+            SqlConnection Connection = new SqlConnection(clsDataAccessLayerSettings.ConnectionString);
+            string Query = @"Select MinimumAllowedAge From LicenseClasses Where LicenseClassID = @LicenseClassID";
+            SqlCommand Command = new SqlCommand(Query, Connection);
+            Command.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+            try
+            {
+                Connection.Open();
+                object Result = Command.ExecuteScalar();
+                if (Result != null && int.TryParse(Result.ToString(), out int Temp))
+                    MinimumAllowedAge = Temp;
+            }
+            catch { }
+            finally { Connection.Close(); }
+            return MinimumAllowedAge;
+        }
+    }
+}
