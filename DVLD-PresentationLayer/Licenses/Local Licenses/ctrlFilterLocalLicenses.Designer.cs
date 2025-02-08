771535ad57a087c387782e53e8175dba87ec8588
eslambob44 <eyasser237@gmail.com>
2025-02-08 16:49:51 +0200

    Implement filter local licenses control

---
+++ DVLD-PresentationLayer/Licenses/Local Licenses/ctrlFilterLocalLicenses.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DVLD_PresentationLayer.Licenses.Local_Licenses
+{
+    partial class ctrlFilterLocalLicenses
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ctrlFilterLocalLicenses));
+            this.ctrlShowLicenseInfo1 = new DVLD_PresentationLayer.Licenses.ctrlShowLocalLicenseInfo();
+            this.gbFilter = new System.Windows.Forms.GroupBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.mtxtFilter = new System.Windows.Forms.MaskedTextBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.gbFilter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ctrlShowLicenseInfo1
+            // 
+            this.ctrlShowLicenseInfo1.BackColor = System.Drawing.Color.White;
+            this.ctrlShowLicenseInfo1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ctrlShowLicenseInfo1.Location = new System.Drawing.Point(0, 104);
+            this.ctrlShowLicenseInfo1.Name = "ctrlShowLicenseInfo1";
+            this.ctrlShowLicenseInfo1.Size = new System.Drawing.Size(890, 399);
+            this.ctrlShowLicenseInfo1.TabIndex = 0;
+            // 
+            // gbFilter
+            // 
+            this.gbFilter.Controls.Add(this.btnFilter);
+            this.gbFilter.Controls.Add(this.mtxtFilter);
+            this.gbFilter.Controls.Add(this.label20);
+            this.gbFilter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gbFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbFilter.Location = new System.Drawing.Point(0, 0);
+            this.gbFilter.Name = "gbFilter";
+            this.gbFilter.Size = new System.Drawing.Size(890, 100);
+            this.gbFilter.TabIndex = 1;
+            this.gbFilter.TabStop = false;
+            this.gbFilter.Text = "Filter";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(208, 35);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(65, 16);
+            this.label20.TabIndex = 51;
+            this.label20.Text = "License:";
+            // 
+            // mtxtFilter
+            // 
+            this.mtxtFilter.Location = new System.Drawing.Point(279, 32);
+            this.mtxtFilter.Mask = "000000000000";
+            this.mtxtFilter.Name = "mtxtFilter";
+            this.mtxtFilter.Size = new System.Drawing.Size(214, 22);
+            this.mtxtFilter.TabIndex = 52;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Image = ((System.Drawing.Image)(resources.GetObject("btnFilter.Image")));
+            this.btnFilter.Location = new System.Drawing.Point(540, 18);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(56, 50);
+            this.btnFilter.TabIndex = 53;
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // ctrlFilterLocalLicenses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gbFilter);
+            this.Controls.Add(this.ctrlShowLicenseInfo1);
+            this.Name = "ctrlFilterLocalLicenses";
+            this.Size = new System.Drawing.Size(890, 503);
+            this.gbFilter.ResumeLayout(false);
+            this.gbFilter.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Licenses.ctrlShowLocalLicenseInfo ctrlShowLicenseInfo1;
+        private System.Windows.Forms.GroupBox gbFilter;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.MaskedTextBox mtxtFilter;
+        private System.Windows.Forms.Button btnFilter;
+    }
+}
---
+++ DVLD-PresentationLayer/Licenses/Local Licenses/ctrlFilterLocalLicenses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_PresentationLayer.Licenses.Local_Licenses
+{
+    public partial class ctrlFilterLocalLicenses : UserControl
+    {
+        public ctrlFilterLocalLicenses()
+        {
+            InitializeComponent();
+        }
+
+        public event Action<int> OccurFilterEvent;
+
+        private void OccurFilterHandlerEvent(int LicenseID)
+        {
+            Action<int> Handler = OccurFilterEvent;
+            if(Handler != null)
+            {
+                Handler(LicenseID);
+            }
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            int LicenseID = int.Parse(mtxtFilter.Text.Trim());
+            bool IsFound = ctrlShowLicenseInfo1.Find(LicenseID);
+            LicenseID = (IsFound) ? LicenseID : -1;
+            OccurFilterHandlerEvent(LicenseID);
+        }
+
+        public bool FilterEnabled { 
+            set
+            {
+                gbFilter.Enabled = value;
+            } 
+        }
+    }
+}
